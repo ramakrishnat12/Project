c46a34107588f8c37ed0be577dcc16fdb12c955a
Rama Krishna <ramakrishnat@live.com>
2022-05-03 14:23:05 -0400

    first upload

---
+++ BallonpopGame/app.js
@@ -0,0 +1,17 @@
+// variable declaration total popped ballons, 
+
+
+// button
+
+
+
+// ballon logic(planning to use random)
+
+// timing for 30Secs
+
+// display score and prompt for player to play whenever they are ready
+
+// display both players score and who won!
+
+// 
+
